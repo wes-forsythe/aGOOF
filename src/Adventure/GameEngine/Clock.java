7271c16ac45b82b2c440cbdbd9112c143150dbfe
aperson40 <jon.calhoun42@gmail.com>
2012-02-23 21:25:12 -0600

    Add JavaDoc to source.

--- src/Adventure/GameEngine/Clock.java
+++ src/Adventure/GameEngine/Clock.java
@@ -6,13 +6,37 @@ package Adventure.GameEngine;
 
 import Adventure.Creatures.ClockWatcher;
 /**
- * Observer Design Patter
+ * Clock interface uses the Observer Design Pattern objects (subscribers)
+ * can subscribe to the clock to be notified of each clock tick.
  * 
- * @author jeffj
+ * @author Jon Calhoun, jeffj
+ * 
+ * @version 20120208-01
  */
 public interface Clock {                    // Subject interface
     
-    public void tick();                     // notifyObservers()
-    public void subscribe(ClockWatcher w);  // registerObserver() 
+    /**
+     *  Outline for the increment of the game clock. All current subscribers
+     * to the clock should be notified of the clock tick when implementing.
+     */
+    public void tick(); 
+    
+    /**
+     * Outline for method to add a new ClockWatcher to the current list of subscribers.
+     * 
+     * @param w ClockerWatcher that is going to subscribing to the clock as an observer.
+     * 
+     * @see ClockWatcher
+     */
+    public void subscribe(ClockWatcher w);  
+    
+    /**
+     * Outline for the method to remove a currently subscribed ClockWatcher from 
+     * being notified of ticks of the clock.
+     * 
+     * @param w ClockWatcher that should not observe the clock at each tick.
+     * 
+     * @see ClockWatcher
+     */
     public void unsubscribe(ClockWatcher w);// removeObserver()
 }
