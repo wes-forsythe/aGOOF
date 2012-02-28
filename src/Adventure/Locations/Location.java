5a680550c9aa4a661abf4ee2116aaec0e1cc9901
Keith Smith <Keith.E.Smith90@gmail.com>
2012-02-27 19:57:08 -0600

    Mostly adding in my JavaDoc comments for the Location class. Honestly seems it is my first JavaDoc please feel free to direct and assist correct so I may learn how to comment better.

--- src/Adventure/Locations/Location.java
+++ src/Adventure/Locations/Location.java
@@ -10,14 +10,22 @@ import Adventure.GameParser.Noun;
  * @author jeffj
  */
 public abstract class Location extends Noun {
-    
+    /**
+     * protected storage for name
+     */    
     protected String name;
-    
+     /**
+     * constructor places name into protected variable.
+     * @param name
+     */   
     public Location(String name) {
         super(name.toLowerCase());
         this.name = name;
     }   
-    
+     /**
+     * access method to protected string variable name
+     * @return 
+     */   
     @Override
     public String toString() {
         return this.name;
