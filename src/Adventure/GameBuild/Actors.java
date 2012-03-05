548cfdc9e1f9a6bb5d0fc9822b3afc741c043093
Chris Jones <christopher.jones@smail.astate.edu>
2012-03-05 16:27:55 -0600

    Javadoc implementation.

--- src/Adventure/GameBuild/Actors.java
+++ src/Adventure/GameBuild/Actors.java
@@ -12,14 +12,35 @@ import Adventure.GameEngine.*;
 /**
  * For building NPC's used in game interaction
  * 
- * @author jeffj
+ * @see Monster
+ * @see Rabble
+ * @see Sidekick
+ * @see ClockWatcher
+ *  
+ * @author Chris Jones, jeffj
+ * 
+ * @version 20120305-01
  */
 public class Actors {
-        
+    
+    /**A monster named Boris */
     public static Monster Boris = new Monster("Boris");
+    /**A rabble named Frank */
     public static Rabble Frank = new Rabble("Frank");
+    /**A sidekick named Robin */
     public static Sidekick Robin = new Sidekick("Robin");
     
+    /**
+     * Initializes locations/movements for the actors in the game<br>
+     * Also adds these actors to the world
+     * 
+     * @see Monster#setLocation(Adventure.Locations.Location) 
+     * @see Rabble#setLocation(Adventure.Locations.Location) 
+     * @see Rabble#setMovement(Adventure.Creatures.MoveAction) 
+     * @see World#addActor(Adventure.Creatures.Creature) 
+     * @see Engine#subscribe(Adventure.Creatures.ClockWatcher)
+     * 
+     */
     public static void Build() {
         
         Boris.setLocation(Cave);
