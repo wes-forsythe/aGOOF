615841890fd776833d5422b62496fe28fd841e71
kevin <kevin@10.101.71.36>
2012-03-05 12:57:15 -0600

    javadocs for article

--- src/Adventure/GameParser/Article.java
+++ src/Adventure/GameParser/Article.java
@@ -7,13 +7,20 @@ package Adventure.GameParser;
 import static Adventure.GameParser.Word.PartsOfSpeech.*;
 /**
  *
- * @author jeffj
+ * @author jeffj, kevinratton
  */
 public class Article extends Word {
+    /**
+     * gets private string word to set type article
+     * @param word string to set type
+     */
     public Article(String word) {
         super(word, ARTICLE);
     }
-
+    /**
+     * calls Word toString() method
+     * @return 
+     */
     @Override
     public String toString() {
         return "AOBJ("+super.toString()+")";
