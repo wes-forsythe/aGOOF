84f5dc5523c69d54b0b0c1802623e2e1552f0e88
tk-banks <tkraybanks@yahoo.com>
2012-03-06 18:12:28 -0600

    javadoc comments added

--- src/Adventure/Adventure.java
+++ src/Adventure/Adventure.java
@@ -18,16 +18,41 @@ import Adventure.GameEngine.*;
  * 
  * @TODO extend the streams to include an output T class for enabling transcripts
  */
+ /**
+ * This is the description/purpose of the Adventure class.
+ * 
+ * @author Tara Banks
+ * @version 20120210-01 
+ * @throws IOException
+ */
+
 public class Adventure {
     /** establishes the input stream for the game */
+        /**
+     * Set input to the standard input stream
+     */
+
     public static final InputStream in = System.in;
     /** establishes the output stream for the game */
+        /**
+     * Set output to the standard output stream
+     */
+
     public static final PrintStream out = System.out;
     
     /** entry point for the game: prints the banner, sets up the IO, and starts play. 
      * @param args the command line arguments
      * @throws IOException
      */
+     /**
+     * Main method prints game information such as title, copyright, version, release and serial.
+     * In addition the the aforementioned tasks, main also creates an instance of the game engine
+     * and initializes game play.
+     * 
+     * @param args the command line arguments
+     * @return nothing
+     */
+
     public static void main(String[] args) throws IOException {
         
         out.printf("%s\n", World.title);
