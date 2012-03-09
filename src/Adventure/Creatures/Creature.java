4d85ddabf8112ca7671dfa2c8ce0a8dc85196864
wes-forsythe <wesforsythe09@gmail.com>
2012-03-08 18:01:23 -0600

    Added JavaDoc to Creature

--- src/Adventure/Creatures/Creature.java
+++ src/Adventure/Creatures/Creature.java
@@ -6,34 +6,48 @@ package Adventure.Creatures;
 
 import Adventure.GameObject.*;
 import Adventure.Locations.*;
-/**
- *
- * @author jeffj
+//** Serves as a basis for monster, rabble, etc.
  * 
- * @TODO move Noun from parent to composite part, implement Label interface
+ *
+ * @author jeffj, wesf
+ * @version 20120305-01
  */
 public abstract class Creature extends GameObject implements ClockWatcher {
-
+    
+    /**creates a moveAction that is based on a particular creature*/
     protected MoveAction movement;
+    
+    /**creates a Location based on a particular creature*/
     protected Location location;
     
+    /**Constructor passes the name of the creature to GameObject
+     *@param name   The name of the specific creature
+     */
     public Creature(String name) {
         super(name);
     }
     
+    /**listens to the watcher, tocks on the tick*/
     @Override
     public void tock() {
         this.movement.move();
     }
     
+    /**changes the movement of the creature
+     *@param movement  the type of movement to be assigned
+     */
     public void setMovement(MoveAction movement) {
         this.movement = movement;
     }
     
+    /**changes the location of the creature
+     *@param location  the location the creature will be assigned to
+     */
     public void setLocation(Location location) {
         this.location = location;
     }
     
+    /**Displays the creatures name, movement type, and location*/
     @Override
     public String toString() {
         return this.name+","+this.movement+","+this.location;
