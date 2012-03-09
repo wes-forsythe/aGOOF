c3644c69652d994baca41022a805626c8a3ac063
Derek Escue <derek.escue@gmail.com>
2012-03-08 20:39:17 -0600

    Updated JavaDoc

--- src/Adventure/GameParser/Parser.java
+++ src/Adventure/GameParser/Parser.java
@@ -11,25 +11,52 @@ import java.util.ArrayList;
 /**
  *
  * @author jeffj
+ * @author dereke
+ * @verson ?
  */
+
+/** @see parse() */
 public class Parser {
+
     private InputStream in = System.in;
     private PrintStream out = System.out;
     private Scanner scanner;
     
+    /** Start the Scanner  */
     public Parser() {
         this.scanner = new Scanner();
     }
     
+    /** Changes the InputStream that the user has typed. 
+     * 
+     * 
+     * @param in InputStream that should be the new in stream for the scanner in parse()
+     * 
+     */
     public void setInput(InputStream in) {
         this.in = in;
     }
     
+    /** Changes the OutputStream. 
+     * 
+     * 
+     * @param out OutputStream that should be the new print stream
+     * 
+     */
     public void setOutput(PrintStream out) {
         this.out = out;
     }
     
-    /*
+    
+    /**   
+     *  Scan the words or characters from the user and parse.
+     *  Put the words in an ArrayList and scan them. 
+     * 
+     * @see Scanner
+     * @see Action
+     * @return parse   returns the new Action to do: parse
+     * @throws IOException  
+
      * @TODO allow ambiguous grammars, i.e., multiple rule matches
      */
     public Action parse() throws IOException {
