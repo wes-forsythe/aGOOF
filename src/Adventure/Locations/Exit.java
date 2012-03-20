07a2c1803ca38f50b978c5e0437562e71f203af0
aperson40 <jon.calhoun42@gmail.com>
2012-03-19 22:52:34 -0500

    Expand/Fill out  Locations Package.

--- src/Adventure/Locations/Exit.java
+++ src/Adventure/Locations/Exit.java
@@ -3,16 +3,48 @@
  * and open the template in the editor.
  */
 package Adventure.Locations;
-
+import Adventure.Things.*;
 /**
  * For defining and initializing exit locations in the game
  *
- * @author Chris Jones, jeffj
+ * @author Jon Calhoun, Chris Jones, jeffj
  * 
  * @version 20120305-01
  * 
- * @TODO Finish building this class and its methods
  */
-public class Exit {
+public class Exit 
+{
+    private Location exitLocation;
+    private boolean viewBlocked;
+    private Thing prop;
+    public Exit(Location l,Thing prop)
+    {
+        this.exitLocation = l;
+        this.prop = prop;
+        if(prop == null)
+            viewBlocked = false;
+        else
+            viewBlocked = true;
+    }
     
+    public Location getLocation()
+    {
+        return exitLocation;
+    }
+    
+    public void setLocation(Location l)
+    {
+        this.exitLocation = l;
+    }
+    
+    public String description()
+    {
+        String des;
+        if(!viewBlocked)
+            des = exitLocation.description();
+        else
+            des = prop.description();
+        
+        return des;
+    }
 }
